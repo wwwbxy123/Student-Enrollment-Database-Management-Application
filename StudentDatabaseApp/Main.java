0eda1eb1418b6cc27bb638ee93f380fb497179e3
xb52 <xb52@cornell.edu>
2018-12-29 00:46:29 -0500

    Initial commit

---
+++ StudentDatabaseApp/Main.java
@@ -0,0 +1,11 @@
+package StudentDatabaseApp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /** Ask how many new students we want to add */
+
+
+        /** create n new students
+    }
+}
---
+++ StudentDatabaseApp/Students.java
@@ -0,0 +1,29 @@
+package StudentDatabaseApp;
+
+public class Students {
+    private String  firstName;
+    private String lastName;
+    private String gradYear;
+    private int ID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCredits = 600;
+
+    /**  constructor: student enters names and school year */
+
+
+    /** generate ID */
+
+
+    /** enroll in courses */
+
+
+    /** view balance */
+
+
+    /** pay tuition */
+
+
+    /** show status */
+
+}
